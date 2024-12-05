15a6832a524c95add941694fe243f6945b9d6762
null221 <agd1923@gmail.com>
2024-12-05 16:58:38 +0100

    Created two methods in class VinylMix for return a string votes and imageUrl to renderize. Generated aleatory inserts in method new of MixController. Replaced and deleted MixRepository service by VinylMixRepository

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\VinylMix;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
+#[Route('/mix')]
 class MixController extends AbstractController
 {
-    #[Route('/mix/new', name: 'app_mix')]
-    public function new(): Response
+    #[Route('/new', name: 'app_mix')]
+    public function new(EntityManagerInterface $em): Response
     {
-        dd('new mix');
+        $mix = new VinylMix();
+        $mix->setTitle('Do you Remember... Phil Collins?!');
+        $mix->setDescription('A pure mix of drummers turned singers!');
+        $genresArray = ['pop', 'rock', 'Heavy Metal'];
+        $mix->setGenre($genresArray[array_rand($genresArray)]);
+        $mix->setTrackCount(rand(5,20));
+        $mix->setVotes(rand(-50,50));
 
-        /* return $this->render('mix/index.html.twig', [
-            'controller_name' => 'MixController',
-        ]); */
+        $em->persist($mix);
+        $em->flush();
+        return new Response(sprintf(
+            'Mix %d is %d tracks of pure 80\'s heaven',
+            $mix->getId(),
+            $mix->getTrackCount()
+        ));
     }
 }
